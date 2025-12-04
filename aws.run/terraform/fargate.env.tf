dc9fb365f5460b8d3850ec6b35a9095e9ad0142c
Xu, Qinying (H&B, Herston) <Qinying.Xu@csiro.au>
2025-12-04 20:13:48 +1000

    TF update

--- aws.run/terraform/fargate.env.tf
+++ aws.run/terraform/fargate.env.tf
@@ -1,6 +1,6 @@
 
 resource "aws_batch_compute_environment" "fargate_environment" {
-  compute_environment_name = "fargate_environment"
+  name = "fargate_environment"
   type                     = "MANAGED"
   service_role         = aws_iam_role.batch_service_role.arn
   depends_on   = [aws_iam_role_policy_attachment.batch_service_policy_attachment]
